8531b1c190eb05d57d75eb68f0cb346b53d4cc89
yangdeyi1201 <67776604+yangdeyi1201@users.noreply.github.com>
2021-01-11 16:48:55 +0800

    Update paths.py

--- config/paths.py
+++ config/paths.py
@@ -14,7 +14,7 @@ TESTS_PATH = Path(__file__).resolve().parents[1]/'tests'
 
 
 ''' 1、获取当前文件绝对路径 '''
-# current_path = Path(__file__).resolve()
+current_path = Path(__file__).resolve()
 
 ''' 2、获取当前文件上 n 级目录 '''
 # current_path.parents[n-1]
